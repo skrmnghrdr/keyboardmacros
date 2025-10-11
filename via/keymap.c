84962e9132572625d948e06b78ba942ca97c6bd5
Silanganan Dimasilang <notpaulogrian@protonmail.com>
2025-10-11 19:45:23 -0400

    fixed panic key addded bin as well
    added the binary and fixed the panic key

--- via/keymap.c
+++ via/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI,  LT(2, KC_LALT),                        KC_SPC,                                KC_RALT,  KC_APP,   KC_RCTL,  MO(3)
     ),
     [1] = LAYOUT_tkl_ansi( //caps layer
-        QK_GESC, KC_F1,     KC_F2,     KC_F3,      KC_F4,      KC_F5,    KC_F6,    KC_F7,   KC_F8,    KC_F9,     KC_F10,     KC_F11,  KC_F12,   KC_DEL,
+        KC_PANIC, KC_F1,     KC_F2,     KC_F3,      KC_F4,      KC_F5,    KC_F6,    KC_F7,   KC_F8,    KC_F9,     KC_F10,     KC_F11,  KC_F12,   KC_DEL,
         KC_TAB,  KC_Q,     KC_W,     KC_E,      KC_R,      KC_T,     KC_Y,     KC_U,    KC_UP,     KC_LBRC,     KC_RBRC,     KC_LBRC,  KC_RBRC,  TG(0),
         KC_NO, LCTL(KC_A),     KC_S,     KC_D,      KC_PGDN,      KC_ENT,     KC_H,     KC_LEFT,    KC_DOWN,     KC_RGHT,     KC_SCLN,  KC_QUOT,            KC_ENT,
         KC_LSFT, KC_Z,     KC_X,     KC_C,      KC_V,      KC_PGUP,     KC_N,     KC_M,    KC_COMM,  KC_DOT,             KC_SLSH,            KC_RSFT,
