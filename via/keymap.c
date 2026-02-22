ec0f78b735da38d86b1e8440d8ae5b36f4cebb09
Silanganan Dimasilang <notpaulogrian@protonmail.com>
2026-02-22 11:18:18 -0500

    save old keymap
    working layer 0 fn key is not toggle to gaming layer (vanilla layer) which has no macros, used for gaming,
    press fn from layer 3 to go back to working layer 0

--- via/keymap.c
+++ via/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,     KC_W,     KC_E,      KC_R,      KC_T,     KC_Y,     KC_U,    KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
         MO(1), KC_A,     KC_S,     KC_D,      KC_F,      KC_G,     KC_H,     KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
         KC_LSFT, KC_Z,     KC_X,     KC_C,      KC_V,      KC_B,     KC_N,     KC_M,    KC_COMM,  KC_DOT,             KC_SLSH,            KC_RSFT,
-        KC_LCTL, KC_LGUI,  LT(2, KC_LALT),                        KC_SPC,                                KC_RALT,  KC_APP,   KC_RCTL,  MO(3)
+        KC_LCTL, KC_LGUI,  LT(2, KC_LALT),                        KC_SPC,                                KC_RALT,  KC_APP,   KC_RCTL,  TG(3) // go to vanilla mode
     ),
     [1] = LAYOUT_tkl_ansi( //caps layer
         KC_PANIC, KC_F1,     KC_F2,     KC_F3,      KC_F4,      KC_F5,    KC_F6,    KC_F7,   KC_F8,    KC_F9,     KC_F10,     KC_F11,  KC_F12,   KC_DEL,
@@ -37,20 +37,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LALT,  KC_LGUI,                        KC_SPC,                                KC_RGUI,  KC_APP,   KC_RCTL,  MO(3)
     ),
     [2] = LAYOUT_tkl_ansi( //alt layer
-        KC_GRV,  KC_F1,    KC_F2,    KC_F3,     LALT(KC_F4),     KC_F5,    KC_F6,    KC_F7,   KC_F8,    KC_F9,    KC_MUTE,   KC_VOLD,   KC_VOLU,   U_EE_CLR,
-        KC_TAB,  KC_1,  KC_2,  KC_3,   KC_4,    KC_5,  KC_6,  KC_7, KC_8,     KC_9,     KC_0,     RGB_SPD,  RGB_SPI,  TG(0),
-        KC_NO, TO(0),    TO(1),    KC_D,      KC_F,      KC_INS,   KC_HOME,  KC_PGUP, KC_MINS,     KC_EQL,     RGB_HUD,  RGB_HUI,            KC_ENT,
-        KC_LSFT, LOGO_MOD, LOGO_HUI, RGB_SAD,   RGB_SAI,   KC_DEL,   KC_END,   KC_PGDN, RGB_VAD,  RGB_VAI,            KC_UP,              QK_BAT,
-        KC_LCTL, KC_LGUI,   KC_NO,                        KC_SPC,                              KC_LEFT,  KC_DOWN,  KC_RGHT,  TG(3)
+        KC_GRV,  KC_F1,    KC_F2,    KC_F3,     LALT(KC_F4),     KC_F5,    KC_F6,    KC_F7,   KC_F8,    KC_F9,    KC_MUTE,   KC_VOLD,   KC_VOLU,   KC_BSPC,
+        KC_TAB,  KC_1,  KC_2,  KC_3,   KC_4,    KC_5,  KC_6,  KC_7, KC_8,     KC_9,     KC_0,     RGB_SPD,  RGB_SPI, RGB_MOD,
+        KC_NO,   MD_BLE1,    MD_BLE2,   MD_BLE3,      RGB_RTOG,      MD_24G,   KC_HOME,  KC_PGUP, KC_MINS,     KC_EQL,     RGB_HUD,  RGB_HUI,            KC_ENT,
+        KC_LSFT, U_EE_CLR, LOGO_MOD, RGB_SAD,   RGB_SAI,   RGB_VAI,   RGB_VAD,   LOGO_HUI, RGB_VAD,  RGB_VAI,            KC_UP,              QK_BAT,
+        KC_LCTL, KC_LGUI,   KC_NO,                        KC_SPC,                                       KC_LEFT,  KC_DOWN,  KC_RGHT,  TG(3)
     ),
     [3] = LAYOUT_tkl_ansi( //fn key layer (keyboard things)
-        KC_PANIC,  KC_1,     KC_2,     KC_3,      KC_4,      KC_5,     KC_6,     KC_MPRV, MD_24G,  MD_BLE3,  TG(0),  MD_BLE1,  MD_BLE2,  U_EE_CLR,
-        KC_TAB,  KC_Q,     KC_W,     KC_E,      KC_R,      KC_T,     KC_Y,  KC_PAUS, KC_I,     TG(0),     KC_P,     RGB_SPD,  RGB_SPI,  RGB_MOD,
-        RGB_RTOG, KC_A,     KC_S,     KC_D,      KC_F,      KC_G,   KC_H,  KC_J, KC_K,     KC_PGUP,  RGB_HUD,  RGB_HUI,            KC_ENT,
-        KC_LSFT, KC_Z,     KC_X,     KC_C,      KC_V,      KC_B,   KC_END,   KC_PGDN, RGB_VAD,  RGB_VAI,            RGB_SAI,              QK_BAT,
-        KC_LCTL, KC_LGUI,  KC_LALT,                        KC_SPC,                              LOGO_MOD,  LOGO_HUI,  RGB_SAD,  KC_NO
-    )
-};
+        QK_GESC, KC_1,     KC_2,     KC_3,      KC_4,      KC_5,     KC_6,     KC_7,    KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
+        KC_TAB,  KC_Q,     KC_W,     KC_E,      KC_R,      KC_T,     KC_Y,     KC_U,    KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
+        KC_CAPS,   KC_A,     KC_S,     KC_D,      KC_F,      KC_G,     KC_H,     KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
+        KC_LSFT, KC_Z,     KC_X,     KC_C,      KC_V,      KC_B,     KC_N,     KC_M,    KC_COMM,  KC_DOT,             KC_SLSH,            KC_RSFT,
+        KC_LCTL, KC_LGUI,  KC_LALT,                        KC_SPC,                                KC_RALT,  KC_APP,   KC_RCTL,  TG(3) //go back to home macros
+    ) //USE TO(x) if you want to go to a layer at this point
+}; 
 //KC_Q,     KC_W,     KC_E,      KC_R,      KC_T,     KC_Y,
 
 
@@ -67,40 +67,24 @@ bool process_record_user(uint16_t keycode, keyrecord_t * record)
 {
     Usb_Change_Mode_Delay = 0;    
     Usb_Change_Mode_Wakeup = false;
-    
+
+    uint8_t current_layer = get_highest_layer(layer_state);
     bool ctrl_held = get_mods() & MOD_BIT(KC_LCTL);
-    //bool alt_held = get_mods() & MOD_BIT(KC_LALT);
-    // CTRL hotkeys
+
     switch(keycode)
     {
-        //! begin filter
-        //alt keys first up here
-        //! end filter
-
-
 
         /* end alt keyes */
         case KC_E:
-            if(ctrl_held)
+            if(ctrl_held && current_layer != 3)
             {
                 ctrl_hotkey(KC_END);
                 return false; // don't send E
             }
-            /*
-            if (record->event.pressed) 
-            {
-                //! alt e
-                if (get_mods() & MOD_BIT(KC_LALT)) 
-                {
-                    tap_code(KC_3);
-                    return false; // Prevent KC_Q from being sent
-                }
-            } 
-            break;
-            */
+
            
         case KC_A:
-            if(ctrl_held)
+            if(ctrl_held && current_layer != 3)
             {
                 ctrl_hotkey(KC_HOME);
                 return false; // don't send A
